1109f03022dd552c5387a6ef7baf497fc50320c4
ntnumartin <heyn@chalmers.se>
2022-04-27 13:12:54 +0200

    Temp reading

---
+++ sketch_apr27a/sketch_apr27a.ino
@@ -0,0 +1,33 @@
+/*********
+PretzelTech
+*********/
+
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+// Data wire is conntec to the Arduino digital pin 4
+#define ONE_WIRE_BUS A1
+
+// Setup a oneWire instance to communicate with any OneWire devices
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature sensor 
+DallasTemperature sensors(&oneWire);
+
+void setup(void)
+{
+  // Start serial communication for debugging purposes
+  Serial.begin(9600);
+  // Start up the library
+  sensors.begin();
+}
+
+void loop(void){ 
+  // Call sensors.requestTemperatures() to issue a global temperature and Requests to all devices on the bus
+  sensors.requestTemperatures(); 
+  
+  Serial.print("Celsius_temp: ");
+  //ByIndex0 refers to the first IC on the wire
+  Serial.println(sensors.getTempCByIndex(0)); 
+  delay(1000);
+}
